04b37415ac99d562728317a2e97c4fb59b7116d0
BerkanAktag <berkan.aktag@student.pxl.be>
2017-10-11 16:55:39 +0200

    Exercise 2 - Step 1,2 and 3 created

--- StarWarsUWP/StarWarsUWP/MainPage.xaml.cs
+++ StarWarsUWP/StarWarsUWP/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using StarWars.DAL;
+using StarWars.Domain;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,11 @@ namespace StarWarsUWP
         public MainPage()
         {
             this.InitializeComponent();
+            IRepository repository = new ApiRepository();
+
+            Movie movie = repository.GetMobieByUrl("api/films/1");
+            MovieGrid.DataContext = movie;
+
         }
     }
 }
