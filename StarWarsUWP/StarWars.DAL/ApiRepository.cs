0682bf2e0542020b5023c54155f40a99fd2f1848
BerkanAktag <berkan.aktag@student.pxl.be>
2017-10-11 16:24:45 +0200

    Add startup for project
    Added the start of the project, with classes from the previous exercise. Contains Step 1, 2 and 3.

---
+++ StarWarsUWP/StarWars.DAL/ApiRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using StarWars.Domain;
+
+namespace StarWars.DAL
+{
+    public class ApiRepository : IRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiRepository()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://swapi.co"),
+            };    
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public Movie GetMobieByUrl(string url)
+        {
+            var movie = new Movie();
+
+            HttpResponseMessage response =
+                _httpClient.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+                movie = response.Content.ReadAsAsync<Movie>().Result;
+            
+            return movie;
+        }
+
+        public IList<Movie> GetAllMovies()
+        {
+            var url = "api/films";
+            var allMovies = new List<Movie>();
+
+            HttpResponseMessage response = _httpClient.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+                allMovies = response.Content.ReadAsAsync<ResultsPage<Movie>>().Result.Results;
+
+            return allMovies;
+        }
+
+        
+    }
+        internal class ResultsPage<T>
+        {
+            public int Count { get; set; }
+            public String Next { get; set; }
+            public string Previous { get; set; }
+            public List<T> Results { get; set; }
+        }
+}
---
+++ StarWarsUWP/StarWars.Domain/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StarWars.Domain
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+
+        [JsonProperty(PropertyName = "episode_id")]
+        public int EpisodeId { get; set; }
+
+        [JsonProperty(PropertyName = "opening_crawl")]
+        public string OpeningCrawl { get; set; }
+
+        public string Director { get; set; }
+
+        public string Producer { get; set; }
+
+        [JsonProperty(PropertyName = "release_date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Planet> Planets { get; set; }
+
+        [JsonProperty(PropertyName = "planets")]
+        public List<string> PlanetUris { get; set; }
+
+    }
+}
